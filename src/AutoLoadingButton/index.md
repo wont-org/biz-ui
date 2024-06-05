896fd6fa5f8b7a8730f41ab3cae9a483215981c0
liukun <919590347@qq.com>
2024-06-05 11:11:24 +0800

    feat(AutoLoadingButton): 支持Generator

--- src/AutoLoadingButton/index.md
+++ src/AutoLoadingButton/index.md
@@ -9,7 +9,7 @@ import { AutoLoadingButton } from '@wont/biz-ui';
 const sleep = (timeout) => {
   return new Promise((res, rej) => {
     setTimeout(() => {
-      res();
+      res('timeout');
     }, timeout);
   });
 };
@@ -18,12 +18,32 @@ export default () => {
   const asyncClick = async () => {
     await sleep(2000);
   };
+  function* fetchGenerator() {
+    const urls = [1, 2];
+    for (const url of urls) {
+      yield sleep(1000);
+    }
+  }
+  const generatorCLick = async () => {
+    const gen = fetchGenerator();
+    // for await (const data of gen) {
+    //   console.log('data :>> ', data);
+    // }
+    let result = gen.next();
+    while (!result.done) {
+      const data = await result.value;
+      console.log(data);
+      result = gen.next();
+    }
+  };
+
   const syncClick = () => {
     console.log('syncClick');
   };
   return (
     <Space>
-      <AutoLoadingButton onClick={asyncClick}>async button</AutoLoadingButton>
+      <AutoLoadingButton onClick={asyncClick}>async promise button</AutoLoadingButton>
+      <AutoLoadingButton onClick={generatorCLick}>async generator button</AutoLoadingButton>
       <AutoLoadingButton onClick={syncClick}>sync button</AutoLoadingButton>
     </Space>
   );
--- src/AutoLoadingButton/index.tsx
+++ src/AutoLoadingButton/index.tsx
@@ -2,33 +2,92 @@ import { useSafeState } from 'ahooks';
 import { Button, ButtonProps } from 'antd';
 import React from 'react';
 
+type Thenable<T> =
+  | Promise<T>
+  | { then: (onfulfilled?: (value: T) => any, onrejected?: (reason: any) => any) => any };
+type GeneratorReturn<T> = Generator<T, void, unknown> | AsyncGenerator<T, void, unknown>;
+
+const isThenable = (thing: any): thing is Thenable<unknown> => {
+  return !!(thing && typeof thing.then === 'function');
+};
+
+const isGenerator = (thing: any): thing is () => GeneratorReturn<unknown> => {
+  return (
+    typeof thing === 'function' &&
+    (thing.constructor.name === 'GeneratorFunction' ||
+      thing.constructor.name === 'AsyncGeneratorFunction')
+  );
+};
+
+const runGenerator = (gen: () => GeneratorReturn<unknown>): Promise<void> => {
+  const iterator = gen();
+  return new Promise((resolve, reject) => {
+    const step = (
+      nextF: () => IteratorResult<unknown, void> | Promise<IteratorResult<unknown, void>>,
+    ) => {
+      let next;
+      try {
+        next = nextF();
+      } catch (e) {
+        return reject(e);
+      }
+      if (next instanceof Promise) {
+        next.then(
+          (resolvedNext) => step(() => resolvedNext),
+          (e) => reject(e),
+        );
+      } else {
+        if (next.done) {
+          return resolve();
+        }
+        Promise.resolve(next.value).then(
+          (v) => step(() => iterator.next(v)),
+          (e) => step(() => iterator.throw!(e)),
+        );
+      }
+    };
+    step(() => iterator.next());
+  });
+};
+
 export default (baseProps: ButtonProps) => {
   const { children, onClick, ...props } = baseProps;
   const [loading, setLoading] = useSafeState(false);
-  const isThenable = (thing) => {
-    return !!(thing && !!thing.then);
-  };
+
   const onClickWrap = async () => {
     if (typeof onClick !== 'function') {
       return;
     }
-    const ret = onClick() as undefined | Promise<unknown>;
+
+    const ret = onClick() as undefined | Thenable<unknown> | (() => GeneratorReturn<unknown>);
+
     if (!ret) {
       return;
     }
-    if (!isThenable(ret)) {
-      return;
+
+    if (isThenable(ret)) {
+      setLoading(true);
+      ret.then(
+        () => {
+          setLoading(false);
+        },
+        (e) => {
+          setLoading(false);
+          return Promise.reject(e);
+        },
+      );
+    } else if (isGenerator(ret)) {
+      setLoading(true);
+      runGenerator(ret).then(
+        () => {
+          setLoading(false);
+        },
+        (e) => {
+          setLoading(false);
+          return Promise.reject(e);
+        },
+      );
     }
-    setLoading(true);
-    ret.then(
-      () => {
-        setLoading(false);
-      },
-      (e) => {
-        setLoading(false);
-        return Promise.reject(e);
-      },
-    );
   };
 
   return (
