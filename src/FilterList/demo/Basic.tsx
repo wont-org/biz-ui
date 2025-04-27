81f5220e04514f05bbb6367089aae0e36d20ea0e
Lk(刘坤) <lk.liu@eclicktech.com.cn>
2025-04-27 17:09:49 +0800

    docs(FilterList): 自定义删除icon

--- src/FilterList/demo/Basic.tsx
+++ src/FilterList/demo/Basic.tsx
@@ -1,7 +1,7 @@
+import { MinusOutlined } from '@ant-design/icons';
 import { FilterList } from '@wont/biz-ui';
 import { FIELD_TYPES, RELATION } from '@wont/biz-ui/FilterList/constant';
 import { validator } from '@wont/biz-ui/FilterList/utils';
-import { IconFont, ICON_TYPE } from '@wont/biz-ui/IconFont/icon';
 import { Button, Card, Form, Space, Switch, Typography } from 'antd';
 import React, { useState } from 'react';
 
@@ -163,7 +163,7 @@ export default () => {
           ]}
         >
           <FilterList
-            deleteIcon={<IconFont type={ICON_TYPE.refresh} />}
+            deleteIcon={<MinusOutlined />}
             conditionSelectProps={{ options: OPTIONS }}
             conditionNumberValueProps={{
               min: 1,
