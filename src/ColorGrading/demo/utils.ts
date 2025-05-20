5ba90d1add0e973774e13ad97764b124560448ed
Lk(刘坤) <lk.liu@eclicktech.com.cn>
2025-05-20 15:39:54 +0800

    chore: 删除无用代码

--- src/ColorGrading/demo/utils.ts
+++ src/ColorGrading/demo/utils.ts
@@ -1,19 +1,5 @@
 import chroma from 'chroma-js';
 
-export const generateColorScale = (
-  min: string,
-  mid: string,
-  max: string,
-  midPosition = 0.2,
-  steps = 10,
-) => {
-  return chroma
-    .scale([min, mid, max])
-    .domain([0, midPosition, 1]) // 定义颜色位置比例
-    .mode('lab')
-    .colors(steps);
-};
-
 export const getColorGrading = ({
   scale,
   domain = [0, 0.5, 1],
